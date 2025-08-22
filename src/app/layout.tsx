3724563450fa87cd2087a08a44a5b0ddd0c9c439
Shashank <shashank2531998@gmail.com>
2025-08-22 17:20:25 +0000

    I also want to setup Google analytics, therefore, do the below step -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "next-themes"
+import Script from 'next/script';
 
 
 export const metadata: Metadata = {
@@ -20,6 +22,16 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V52571H7YQ"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-V52571H7YQ');
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
