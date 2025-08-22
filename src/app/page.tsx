357f3dd89728cc3f15a13310ff6f02d3bef34e8a
Shashank <shashank2531998@gmail.com>
2025-08-22 12:54:51 +0000

    The timeline centres should be out of the card and should be placed on t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -425,16 +425,16 @@ function ExperienceGroupCard({ item }: { item: any }) {
                     </div>
                 </div>
 
-                <div className="relative pl-6">
-                     <div className="absolute left-[1.625rem] top-2 bottom-2 w-0.5 bg-border"></div>
+                <div className="relative pl-8">
+                    <div className="absolute left-4 top-5 bottom-5 w-0.5 bg-border -translate-x-1/2"></div>
                     {item.roles.map((role: any, index: number) => (
                         <Dialog key={index}>
                             <DialogTrigger asChild>
-                                <div className="relative py-4 group cursor-pointer">
-                                    <div className="absolute left-0 top-7 h-4 w-4 rounded-full bg-background border-2 border-primary group-hover:scale-110 transition-transform flex items-center justify-center">
+                                <div className="relative flex items-start py-4 group cursor-pointer">
+                                    <div className="absolute left-4 top-5 h-4 w-4 rounded-full bg-background border-2 border-primary group-hover:scale-110 transition-transform -translate-x-1/2 flex items-center justify-center">
                                        <div className="h-1.5 w-1.5 rounded-full bg-primary transition-colors"></div>
                                     </div>
-                                     <div className="p-4 rounded-md transition-all duration-200 hover:bg-accent pl-12">
+                                     <div className="p-4 rounded-md transition-all duration-200 hover:bg-accent flex-grow ml-8">
                                         <div className="flex justify-between items-start">
                                             <div>
                                                 <h4 className="font-semibold font-headline text-foreground">{role.role}</h4>
